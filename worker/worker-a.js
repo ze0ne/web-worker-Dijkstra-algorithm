3bbae3a16d7e98bc6ed8312edf3675c560fb729e
ze0ne <romain.guinand@gmail.com>
2024-03-19 00:53:35 +0100

    wip

--- worker/worker-a.js
+++
@@ -1,7 +0,0 @@
-onmessage = e => {
-  const { username, password } = e.data;
-  console.log('Worker A');
-  if (password.toString().length > 6) {
-    postMessage({ cmd: 'passwordIsValid', isValid: true });
-  }
-};
--- worker/worker-b.js
+++
@@ -1,5 +0,0 @@
-onmessage = e => {
-  const { username } = e.data;
-  const isValid = !/[!@#$%]/.test(username);
-  postMessage({ cmd: 'usernameIsValid', isValid });
-};
